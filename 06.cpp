eedbb5fba722700b691fc78fd1ede23fd9221b5e
JiangXiang1925 <32732276+JiangXiang1925@users.noreply.github.com>
2019-08-21 14:55:43 +0800

    Create 06.cpp

---
+++ 06.cpp
@@ -0,0 +1,37 @@
+struct ListNode
+{
+    int m_nValue;
+    ListNode* m_pNext;
+}
+
+//****************1****************
+void PrintListReversing_Iteratively(ListNode* pHead)
+{
+    std::stack<ListNode>nodes;
+    
+    ListNode* pNode = pHead;
+    while(pNode!=nullptr) 
+    {
+      nodes.push(pNode);
+      pNode=pNode->m_pNext;
+    }
+    
+    while(!nodes.empty())
+    {
+        pNode = nodes.top();
+        printf("%d\t",pNode->m_nValue);
+        nodes.pop();
+    }
+}
+//****************2****************
+void PrintListReversing_Iteratively(ListNode* pHead)
+{
+    if(pHead!=nullptr)
+    {
+        if(pHead->m_pNext!=nullptr)
+        {
+            PrintListReversing_Iteratively(pHead->m_pNext);
+        }
+        printf("%d\t",pHead->m_nValue);
+    }
+}
