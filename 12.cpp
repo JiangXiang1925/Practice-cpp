c1a5535b6fae5fe44851aff606acda39ba942b81
JiangXiang1925 <32732276+JiangXiang1925@users.noreply.github.com>
2019-08-22 15:48:40 +0800

    Create 12.cpp

---
+++ 12.cpp
@@ -0,0 +1,9 @@
+bool hasPath(char*matix,int rows,int cols,char* str)
+{
+    if(matrix == nullptr || rows<1 || cols<1 || str == nullptr)
+        return false;
+        
+    bool *visited = new bool[rows*cols];
+    memset()
+    //待续
+}
