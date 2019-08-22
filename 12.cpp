19c8f6c1868bd92634840680f30fb67f023c10fb
JiangXiang1925 <32732276+JiangXiang1925@users.noreply.github.com>
2019-08-22 16:35:55 +0800

    Update 12.cpp

--- 12.cpp
+++ 12.cpp
@@ -4,6 +4,48 @@ bool hasPath(char*matix,int rows,int cols,char* str)
         return false;
         
     bool *visited = new bool[rows*cols];
-    memset()
-    //待续
+    memset(visited,0,rows*cols);
+    
+    int pathLength = 0;
+    for(int row = 0;row<rows;++row)
+    {
+        for(int col = 0;col<cols;++col)
+        {
+            if(hasPathCore(matrix,rows,cols,row,col,str,pathLength,visited))
+            {
+                return true
+            }
+        }
+    }
+    delete[] visited;
+    
+    return false;
+}
+
+bool hasPathCore(const char*matrix,int rows,int cols,int row,int col,const char* str,int& pathLength,bool* visited)
+{
+    if(str[pathLength]=='\0')
+        return true;
+    
+    bool hasPath = false;
+    if(row>=0 && row<rows && col>=0 && clo<cols
+      && matrix[row*cols+col] == str[pathLength]
+      && !visited[row*cols+col])
+    {
+        ++pathLength;
+        visited[row*cols+col] = true;
+        
+        hasPath = hasPathCore(matrix,rows,cols,row,col-1,str,pathLength,visited)
+            || hasPathCore(matrix,rows,cols,row-1,col,str,pathLength,visited)
+            || hasPathCore(matrix,rows,cols,row,col+1,str,pathLength,visited)
+            || hasPathCore(matrix,rows,cols,row+1,col,str,pathLength,visited)
+        
+        if(!hasPath)
+        {
+            --pathLength;
+            visited[row*cols+col] = false;
+        }
+    }
+    
+    return hasPath;
 }
